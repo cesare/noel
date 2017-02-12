6f5b33bdaa04ac8bf1a1c786ab84df263b01f904
SAWADA Tadashi <cesare@mayverse.jp>
2017-02-12 22:34:36 +0900

    Add AMQP publisher

---
+++ lib/noel/amqp_queue.ex
@@ -0,0 +1,17 @@
+
+defmodule Noel.AmqpQueue do
+  def publish(title, text) do
+    {:ok, message} = create_message(title, text)
+    publish_message(message)
+  end
+
+  defp create_message(title, text) do
+    JSON.encode(%{title: title, text: text})
+  end
+
+  defp publish_message(message) do
+    {:ok, conn} = AMQP.Connection.open
+    {:ok, chan} = AMQP.Channel.open(conn)
+    AMQP.Basic.publish(chan, "noel", "", message)
+  end
+end
