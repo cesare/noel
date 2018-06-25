4eccf1c31ca9b0526e5ee6e762a611df100c302e
SAWADA Tadashi <cesare@mayverse.jp>
2018-06-26 01:04:21 +0900

    Update Elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Noel.Mixfile do
   def project do
     [app: :noel,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.6.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
