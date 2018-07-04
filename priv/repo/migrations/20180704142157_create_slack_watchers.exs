d693295f279c2540e39b9e12327ee3aa9217eac3
SAWADA Tadashi <cesare@mayverse.jp>
2018-07-04 23:58:35 +0900

    Add slack_watchers schema

---
+++ priv/repo/migrations/20180704142157_create_slack_watchers.exs
@@ -0,0 +1,20 @@
+defmodule Noel.Repo.Migrations.CreateSlackWatchers do
+  use Ecto.Migration
+
+  def up do
+    create table("slack_watchers") do
+      add :name,           :string, null: false
+      add :slack_token_id, :integer, null: false
+      add :active,         :boolean, null: false, default: false
+
+      timestamps type: :utc_datetime
+    end
+
+    create index("slack_watchers", ["name"], unique: true)
+  end
+
+  def down do
+    drop index("slack_watchers", ["name"])
+    drop table("slack_watchers")
+  end
+end
